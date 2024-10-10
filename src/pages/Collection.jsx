aa35910162dbfc426518cdffbd1cda63544a6066
Aashish Pandey <pandey321aashish@gmail.com>
2024-10-10 12:52:59 +0530

    Creating basic Search functionality

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from '../components/Title';
 import ProductItems from '../components/ProductItems';
 
 const Collection = () => {
-  const {products}=useContext(ShopContext)
+  const {products,search,showSearch}=useContext(ShopContext)
   const [showFilter,setShowFilter]=useState(false);
   const[filterProduct,setFilterProduct]=useState([]);
   const[category,setCategory]=useState([]);
@@ -31,6 +31,10 @@ const Collection = () => {
   }
   const applyFilter=()=>{
     let productsCopy=products.slice();
+if(showSearch && search){
+  productsCopy=productsCopy.filter(item=>item.name.toLowerCase().includes(search.toLowerCase()))
+}
+
     if(category.length>0){
       productsCopy=productsCopy.filter(item=>category.includes(item.category));
     }
@@ -60,7 +64,7 @@ const Collection = () => {
 
  useEffect(()=>{
     applyFilter();
- },[category,subCategory])
+ },[category,subCategory,search,showSearch])
 
  useEffect(()=>{
   shortProduct()

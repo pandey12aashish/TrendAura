5f94fa308fb17af907701dd3128102125d516870
Aashish Pandey <pandey321aashish@gmail.com>
2024-10-13 09:02:36 +0530

    Create Contact Section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className=''>
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'CONTACT'} text2={"US"}/>
+      </div>
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+         <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-600'>Our Store </p>
+          <p className='text-gray-500'>777 Aamghat <br />Ghazipur,Uttar Pardesh,233001</p>
+          <p className='text-gray-500'>Phon:9140401510 <br />Email:pandey@gmail.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
+          <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, animi.</p>
+           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore More</button>
+         </div>
+      </div>
+      <NewsletterBox/>
+    </div>
   )
 }
 
